ae385b44f3a5633cf308d5336da487b2833e39dd
Your Name <you@example.com>
2020-06-11 14:13:38 +1200

    added base template

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Hello, Flask!"
-    
+    return render_template('home.html')
+
+@app.errorhandler(404)
+def error404(e):
+    #note that we set the 404 status explicity
+    return reader_template('404_html'), 404
+        
