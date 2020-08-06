3b055eb6f702a1d731f8b3c9450eaac5c63e443d
Your Name <you@example.com>
2020-08-06 13:27:37 +1200

    Started login and register form

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+
 @app.route("/")
+def login():
+    return render_template('login.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/home")
 def home():
     return render_template('home.html')
 
@@ -8,5 +17,6 @@ def home():
 def error404(e):
     #note that we set the 404 status explicity
     return render_template('404.html'), 404
-    if __name__ == '__main__':
-  app.run(debug=True)    
+
+if __name__ == '__main__': 
+    app.run(debug=True)    
